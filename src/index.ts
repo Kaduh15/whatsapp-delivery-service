92457328a9448d6d470ae860fe98537f5699eeb1
Kadu <kadu.silva2014@gmail.com>
2024-03-30 19:47:57 -0300

    Feat: configura o primero comando do bot
    instalação do whatsapp-web.js e configuração do primeiro comando do bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello, world!')
+import qrcode from 'qrcode-terminal'
+import { Client, LocalAuth } from 'whatsapp-web.js'
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+})
+
+client.on('auth_failure', () => {
+  console.log('Erro de autenticação')
+})
+
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true })
+})
+
+client.on('authenticated', async () => {
+  console.log('Autenticado com sucesso!')
+})
+
+client.on('ready', async () => {
+  console.log('Client is ready!')
+  await client.sendMessage(
+    client.info.wid._serialized,
+    'Bot iniciado com sucesso!',
+  )
+})
+
+client.on('message', async (msg) => {
+  if (msg.body === '!ping') {
+    msg.reply('pong')
+  }
+})
+
+client.initialize()
